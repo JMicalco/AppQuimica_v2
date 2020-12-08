e88f13ac688057982169d844ee8ea7c19c83520c
Javier González <doniagv@gmail.com>
2020-12-07 18:26:41 -0600

    corrección tabla

--- chemestry-app/src/components/modalFL.js
+++ chemestry-app/src/components/modalFL.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import obj from "../clases/metodos";
 
-obj.orquestador(0,"","","","","",true);
-var arreglo=obj.orquestador("*","","","","","",true);
+obj.orquestador(0, "", "", "", "", "", true);
+var arreglo = obj.orquestador("*", "", "", "", "", "", true);
 
 function createData(name, fracciones) {
-  return { name, fracciones};
+  return { name, fracciones };
 }
 
 const rows = [
-  createData(arreglo[0].x,arreglo[0].x2),
-  createData(arreglo[1].x,arreglo[1].x2),
-  createData(arreglo[2].x,arreglo[2].x2),
-  createData(arreglo[3].x,arreglo[3].x2),
-  createData(arreglo[4].x,arreglo[4].x2),
-  createData(arreglo[5].x,arreglo[5].x2),
-  createData(arreglo[6].x,arreglo[6].x2),
-  createData(arreglo[7].x,arreglo[7].x2),
-  createData(arreglo[8].x,arreglo[8].x2),
-  createData(arreglo[9].x,arreglo[9].x2),
-  createData(arreglo[10].x,arreglo[10].x2),
+  createData(arreglo[0].x, arreglo[0].x2),
+  createData(arreglo[1].x, arreglo[1].x2),
+  createData(arreglo[2].x, arreglo[2].x2),
+  createData(arreglo[3].x, arreglo[3].x2),
+  createData(arreglo[4].x, arreglo[4].x2),
+  createData(arreglo[5].x, arreglo[5].x2),
+  createData(arreglo[6].x, arreglo[6].x2),
+  createData(arreglo[7].x, arreglo[7].x2),
+  createData(arreglo[8].x, arreglo[8].x2),
+  createData(arreglo[9].x, arreglo[9].x2),
+  createData(arreglo[10].x, arreglo[10].x2),
 ];
 function rand() {
   return Math.round(Math.random() * 20) - 10;
@@ -47,15 +47,19 @@ function getModalStyle() {
 
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
-    width: 500,
+    margin: "auto",
+    width: 600,
+
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
   table: {
-    minWidth: 650,
+    minWidth: 600,
+  },
+  tableContainer: {
+    fontSize: "5rem",
   },
 }));
 
@@ -74,31 +78,35 @@ export default function SimpleModal(props) {
   };
 
   const body = (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>{props.nameG}</TableCell>
-            <TableCell align="left">Fracciones</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="left">{row.fracciones}</TableCell>
+    <Paper className={classes.paper}>
+      <TableContainer className={classes.tableContainer}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>{props.nameG}</TableCell>
+              <TableCell align="center">Fracciones</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.fracciones}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 
   return (
     <div>
-      <button type="button" onClick={handleOpen}> Fase Líquida
+      <button type="button" onClick={handleOpen}>
+        {" "}
+        Fase Líquida
       </button>
       <Modal
         open={open}
